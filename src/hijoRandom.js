e3d7ca0045e89a017198067ea4b2fd029416f0c3
Gustavo Boniscontro <guscaballito@gmail.com>
2022-07-19 19:20:53 -0300

    fin

--- src/hijoRandom.js
+++ src/hijoRandom.js
@@ -7,12 +7,26 @@ let result = [];
 
 console.log(getRandomInt(1000))
 for (let i = 0; i < cant; i++) {
-    const r = getRandomInt(1000)
+    const r = parseInt(getRandomInt(1000))
     if (result[r] > 0)
         result[r] += 1
     else
         result[r] = 1
 
 }
+//hay que pasarlo a un objeto para devolverlo la forma mas simple es usar spread y encerrarlo entre llaves de objeto o se puede usar el reduce
 
-process.send(JSON.stringify(result.slice(1)))
+let objRandom = { ...result }
+
+/*
+objRandom = result.slice(1).reduce(function (obj, v) {
+
+    obj[i] = v;
+
+    i++;
+
+    return obj;
+
+}, {});*/
+
+process.send(JSON.stringify(objRandom))
